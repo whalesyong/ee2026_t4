41a9b97af80e441502c0c63fc8e1a051f6f1c933
Lawrence <nullawrence@gmail.com>
2025-03-25 08:37:56 +0800

    created FDP_main for final submission

---
+++ FDP_main/FDP.srcs/sources_1/new/basic_snake.v
@@ -0,0 +1,51 @@
+`timescale 1ns / 1ps
+module basic_snake(
+    input slow_clk,
+    input signed [12:0] x_vel, y_vel,
+    input [8:0] xpos, ypos,
+    output reg [8:0] new_xpos, new_ypos, 
+    output reg signed [12:0] new_x_vel = 0, new_y_vel = 0
+);
+    //note that the box location is now the top-left corner of a 5x5 box
+    localparam MAX_X = 95;
+    localparam MAX_Y = 63;
+    localparam BOX_SIZE = 4;  // 5x5 box (0 to 4)
+
+    reg signed [12:0] potential_xpos, potential_ypos;
+    reg [2:0] flag = 0;
+
+    always @ (posedge slow_clk) begin 
+        flag <= (flag == 7) ? 0 : flag + 1;
+
+        if (flag == 0) begin 
+            new_x_vel = x_vel;
+            new_y_vel = y_vel; 
+            // Update positions using new velocities
+            potential_xpos = $signed(xpos) + new_x_vel;
+            potential_ypos = $signed(ypos) + new_y_vel;
+        end
+
+        // Collision detection for top-left corner positioning
+        if (potential_xpos <= 0) begin  // Left wall
+            potential_xpos = 0;
+            new_x_vel = -new_x_vel;
+        end
+        else if (potential_xpos >= MAX_X - BOX_SIZE) begin  // Right wall
+            potential_xpos = MAX_X - BOX_SIZE;
+            new_x_vel = -new_x_vel;
+        end
+
+        if (potential_ypos <= 0) begin  // Top wall
+            potential_ypos = 0;
+            new_y_vel = -new_y_vel;
+        end
+        else if (potential_ypos >= MAX_Y - BOX_SIZE) begin  // Bottom wall
+            potential_ypos = MAX_Y - BOX_SIZE;
+            new_y_vel = -new_y_vel;
+        end
+        //output the last valid position 
+        new_xpos = potential_xpos[8:0];
+        new_ypos = potential_ypos[8:0];
+    end   
+endmodule
+
---
+++ FDP_main/FDP.srcs/sources_1/new/test_snake.v
@@ -0,0 +1,158 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 21.03.2025 21:00:48
+// Design Name: 
+// Module Name: entire_snake
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+// this is a module showing how to instantiate the basic snake 
+
+module test_snake(
+        input clk, 
+        input btnU, btnD, btnL, btnR,
+        output [7:0]JB
+     );
+     
+     
+     reg [15:0] pixel_colour;
+     wire [12:0] pixel_index;
+     wire [7:0] pixel_x;   
+     wire [7:0] pixel_y;
+     //for oled 
+     wire sending_pixel;
+     wire sample_pixel;
+     PixelToXY converter_mod (
+         .pixel_index(pixel_index),
+         .x(pixel_x),
+         .y(pixel_y)
+     );
+ 
+     parameter MAX_VEL = 5;
+     parameter WHITE = 16'b11111_111111_11111;
+     //6.25MHz for OLED display 
+     wire clk_6p25m;
+     wire clk_25m;
+     wire clk_200h;
+     wire frame_beg;     
+     
+     reg signed [12:0] x_vel;
+     reg signed [12:0] y_vel;
+     
+     wire signed [12:0] new_x_vel;
+     wire signed [12:0] new_y_vel;
+     // position of snake 
+     reg [8:0] xpos;
+     reg [8:0] ypos;
+     
+     wire [8:0] xpos_wire;
+     wire [8:0] ypos_wire;
+  
+     
+     flexible_clk clk_mod25m(
+         .clk(clk),
+         .m(2), 
+         .output_wire(clk_25m)
+     );
+     
+     flexible_clk clk_mod6p25m(
+         .clk(clk),
+         .m(8),
+         .output_wire(clk_6p25m)
+     );
+ 
+     flexible_clk clk_mod200h(
+         .clk(clk),
+         .m(250000),
+         .output_wire(clk_200h)
+     );
+     
+
+     
+     basic_snake snake_mod(
+        .slow_clk(clk_200h),
+        .x_vel(x_vel), 
+        .y_vel(y_vel),
+        .xpos(xpos), 
+        .ypos(ypos), 
+        .new_xpos(xpos_wire), .new_ypos(ypos_wire),
+        .new_x_vel(new_x_vel), 
+        .new_y_vel(new_y_vel)
+     );
+         //inst display 
+     Oled_Display display_mod (  .clk(clk_6p25m),
+                                 .reset(0), 
+                                 .frame_begin(frame_beg),
+                                 .sending_pixels(sending_pixel),
+                                 .sample_pixel(sample_pixel),
+                                 .pixel_index(pixel_index), // output
+                                 .pixel_data(pixel_colour),   
+                                 .cs(JB[0]),
+                                 .sdin(JB[1]),
+                                 .sclk(JB[3]),
+                                 .d_cn(JB[4]),
+                                 .resn(JB[5]), 
+                                 .vccen(JB[6]),
+                                 .pmoden(JB[7])                            
+                               );
+          
+    //init values                     
+    initial begin 
+        xpos = 45; 
+        ypos = 30; 
+        
+        pixel_colour <= 16'h0000; //black 
+        
+        x_vel = 0; 
+        y_vel = 0;
+     
+    end
+
+    
+
+    //update vels with new vels
+    always @ (posedge clk_200h) begin
+        x_vel <= new_x_vel;
+        y_vel <= new_y_vel;
+        
+        // X velocity control with limits
+        if(btnL && x_vel > - MAX_VEL)
+            x_vel <= x_vel - 1;
+        if(btnR && x_vel < MAX_VEL)
+            x_vel <= x_vel + 1;
+            
+        // Y velocity control with limits    
+        if(btnU && y_vel > -MAX_VEL)
+            y_vel <= y_vel - 1;
+        if(btnD && y_vel < MAX_VEL)
+            y_vel <= y_vel + 1;
+    end
+   
+    
+    always @ (posedge clk_25m) begin 
+        xpos <= xpos_wire; 
+        ypos <= ypos_wire;
+        // Changed comparison to use top-left corner as reference
+        if (pixel_x > xpos && pixel_x <= xpos+4 && pixel_y > ypos && pixel_y <= ypos+4)
+            pixel_colour <= 16'b11111_000000_00000; // Red color for snake
+        else 
+            pixel_colour <= 16'h0000;       
+    end
+ 
+    
+    
+endmodule
+
