c8a9f5e11af24cfa693bd86d346efa822e3f4e76
Lawrence <nullawrence@gmail.com>
2025-03-25 08:52:53 +0800

    added constraints

--- FDP_main/FDP.srcs/sources_1/new/Top_Student.v
+++ FDP_main/FDP.srcs/sources_1/new/Top_Student.v
@@ -1,17 +1,13 @@
 `timescale 1ns / 1ps
 
-//////////////////////////////////////////////////////////////////////////////////
-//
-//  FILL IN THE FOLLOWING INFORMATION:
-//  STUDENT A NAME: 
-//  STUDENT B NAME:
-//  STUDENT C NAME: 
-//  STUDENT D NAME:  
-//
-//////////////////////////////////////////////////////////////////////////////////
-
-
-module Top_Student ();
+module Top_Student (  input clk,
+                        input btnU, btnD, btnC, btnL, btnR,
+                        input [15:0] sw,
+                        output [7:0]JB,
+                        output reg [15:0] led,
+                        output [7:0] seg,
+                        output [3:0] an
+                    );
             
 
 
